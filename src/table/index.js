48bec629be9779a277357effa03a2da45440ea8f
Robert Stacey <rjstacey@gmail.com>
2021-03-22 11:50:33 -0700

    reorg

---
+++ src/table/index.js
@@ -0,0 +1,7 @@
+import AppTable from './AppTable';
+import ShowFilters from './ShowFilters'
+import {ControlHeader, ControlCell} from './ControlColumn'
+import ColumnDropdown from './ColumnDropdown'
+
+export default AppTable;
+export {ControlHeader, ControlCell, ColumnDropdown, ShowFilters}
