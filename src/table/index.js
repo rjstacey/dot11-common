01190e711c9b00ef7f3172f124e0b95e9d456f13
Robert Stacey <rjstacey@gmail.com>
2021-04-22 08:03:11 -0700

    added IdList

--- src/table/index.js
+++ src/table/index.js
@@ -3,6 +3,7 @@ import ShowFilters from './ShowFilters'
 import {ControlHeader, ControlCell} from './ControlColumn'
 import ColumnDropdown from './ColumnDropdown'
 import ColumnSelector from './ColumnSelector'
+import {IdSelector, IdFilter} from './IdList'
 
 export default AppTable;
-export {ControlHeader, ControlCell, ColumnDropdown, ColumnSelector, ShowFilters}
+export {ControlHeader, ControlCell, ColumnDropdown, ColumnSelector, ShowFilters, IdSelector, IdFilter}
